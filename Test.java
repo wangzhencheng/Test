a687d47fae2d60ca42861a685ecc487a49eae8ab
wangzhencheng <123456>
2018-06-15 15:04:31 +0800

    's'

--- Test.java
+++ Test.java
@@ -1,8 +1,12 @@
 import java.io.File;
+import java.util.Arrays;
 
 public class Test{
 	
 	public static void main(String args[]){
+		System.out.println("params:"+ Arrays.asList(args));
+		
+		System.out.println("path:"+new File(args[0]).getAbsolutePath());
 		System.out.println("path:"+new File(args[0]).getAbsolutePath());
 	}
 	
