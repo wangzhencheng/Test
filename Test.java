e60dd4eaac10f44c8f21794c163d29981395d362
wangzhencheng <123456>
2018-06-15 15:00:28 +0800

    edit Test 2

--- Test.java
+++ Test.java
@@ -1,9 +1,9 @@
+import java.io.File;
+
 public class Test{
 	
 	public static void main(String args[]){
-		System.out.println("hello:"+args[0]);
-		System.out.println("hello:"+args[0]);
-		
+		System.out.println("path:"+new File(args[0]).getAbsolutePath());
 	}
 	
 }
